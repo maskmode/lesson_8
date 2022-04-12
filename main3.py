a5afe3b56bab13db6d5394acde5b9d823a0ae79e
Admin <chesnokovaleks001@gmail.com>
2022-04-12 16:21:58 +0300

    Done

---
+++ main3.py
@@ -0,0 +1,25 @@
+class Error:
+    def __init__(self, *args):
+        self.my_list = []
+
+    def my_input(self):
+
+        while True:
+            try:
+                val = int(input('Введите значение и нажмийте Enter - '))
+                self.my_list.append(val)
+                print(f'Текущий список - {self.my_list} \n ')
+            except:
+                print(f"Недопустимое значение - введите число")
+                yorn = input(f'Попробовать еще раз? Y/N ')
+
+                if yorn == 'Y' or yorn == 'y':
+                    print(try_except.my_input())
+                elif yorn == 'N' or yorn == 'n':
+                    return f'Вы вышли'
+                else:
+                    return f'Вы вышли'
+
+
+try_except = Error(1)
+print(try_except.my_input())
